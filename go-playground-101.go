39b56430393f014c0d4835535a2b7ea6bd9625fb
Purinat Sanbundit <fame3js49@gmail.com>
2023-03-23 21:52:27 +0700

    [feat] test main go

---
+++ go-playground-101.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+
+func main() {
+    fmt.Println(quote.Go())
+    fmt.Println(testLog())
+}
+
+func testLog() string{
+    return "> test log"
+}
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message
+}
